6daa2b1354a8c1483d6cf41f2439411fedcb7a95
rohit7008 <rohitpottavathini@DESKTOP-63QGIEH.localdomain>
2023-03-05 23:43:44 +0530

    first commit

---
+++ src/lib/PostCard.svelte
@@ -0,0 +1,22 @@
+<script>
+	/**
+	 * @type {any}
+	 */
+	export let title;
+	/**
+	 * @type {any}
+	 */
+	export let description;
+	/**
+	 * @type {any}
+	 */
+	export let href;
+
+</script>
+
+<div class="p-4">
+	<h2 class="text-xl font-semibold">{title}</h2>
+	<p class="text-gray-700 mb-2.5">{description}</p>
+	<a class="font-medium hover:underline" {href}>Read More</a>
+
+</div>
